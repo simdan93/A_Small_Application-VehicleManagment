0fe3934f94a128e208f28c632939760b272d20fd
Daniel Simonsen <sim.danielsen@gmail.com>
2017-09-16 01:04:13 +0200

    change

--- SmallConsoleJSONApp/SmallConsoleJSONApp/Properties/AssemblyInfo.cs
+++ SmallConsoleJSONApp/SmallConsoleJSONApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SmallConsoleJSONApp_ABAX")]
+[assembly: AssemblyTitle("SmallConsoleJSONApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SmallConsoleJSONApp_ABAX")]
+[assembly: AssemblyProduct("SmallConsoleJSONApp")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("61616112-126a-4f7a-8fd4-1afd8fd7a624")]
+[assembly: Guid("181b1ef3-7dcf-432f-86b9-d60dda667e0f")]
 
 // Version information for an assembly consists of the following four values:
 //
